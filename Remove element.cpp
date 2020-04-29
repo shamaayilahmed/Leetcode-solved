8fa6c2d506b959c8dc9fbc2b05494399d9503b24
Shamaayil ahmed <55784371+shamaayilahmed@users.noreply.github.com>
2020-04-29 08:40:23 +0530

    Delete Remove element.cpp

--- Remove element.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        return distance (nums.begin(), remove (nums.begin(),nums.end(),val)); //return size after removing given element
-    }
-};
